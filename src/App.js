195b5840a697ab377ff2380115f10b4e87dce739
harshinipriyasadula-5311 <harshinipriyasadula@gmail.com>
2023-11-06 11:07:47 +0530

    commit msg

--- src/App.js
+++ src/App.js
@@ -61,22 +61,3 @@ export default Data
 
 
 
-// import React,{useState} from 'react';
-// import './App.css';
-
-
-// function App() {
-//   const [countval,setCount]=useState(0);
-//   return (
-//     <div className='div1'>
-//     <div>
-//     <h1>Counter</h1>
-//     <h2>{countval}</h2>
-//     <button  className="btn"  onClick={()=> setCount(countval + 1)}><b>Increment value</b></button>
-//     <button className ="btn"  onClick={()=> setCount(countval - 1)}><b>Decrement value</b></button>
-//     </div>
-//     </div>
-//   );
-// }
-
-// export default App;
